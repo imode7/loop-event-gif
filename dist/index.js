9d4336fc2b65ad9c2bafa3a1006497f567319665
PC-10121024\hoon <imode7@naver.com>
2022-10-17 13:52:33 +0900

    npm publish

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Gif", {
+  enumerable: true,
+  get: function get() {
+    return _Gif.default;
+  }
+});
+var _Gif = _interopRequireDefault(require("../Gif"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
